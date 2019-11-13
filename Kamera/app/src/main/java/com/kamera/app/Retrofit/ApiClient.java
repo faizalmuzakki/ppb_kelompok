bf0a4f8ade4a58d799c74847bd62067e67389b2d
Solork <faizal.muzakki@gmail.com>
2019-11-14 00:54:25 +0700

    add header on post

--- Kamera/app/src/main/java/com/kamera/app/Retrofit/ApiClient.java
+++ Kamera/app/src/main/java/com/kamera/app/Retrofit/ApiClient.java
@@ -5,11 +5,13 @@ import com.kamera.app.Response.ApiResponse;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Url;
 
 public interface ApiClient {
     @FormUrlEncoded
+    @Headers("Accept: application/json")
     @POST
     Call<ApiResponse> store_image(
             @Url String url,
